4119bc50ebb52f0b5a530dfba3afea7c71aa7d8c
Jorge <tuzmoreno.jorgeluis@gmail.com>
2026-06-11 06:46:51 -0600

    primera propuesta de cambio de login con correccion para pasar las pruebas

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react-native';
 import React from 'react';
 import LoginScreen from '../app/login';
 
-// 1. MOCK DE EXPO ROUTER (Ya lo tenías, ¡bien!)
+// 1. MOCK DE EXPO ROUTER
 jest.mock('expo-router', () => ({
     useRouter: () => ({
         replace: jest.fn(),
@@ -12,19 +12,22 @@ jest.mock('expo-router', () => ({
     },
 }));
 
-// 2. MOCK DE ASYNC STORAGE (¡ESTO ES LO QUE FALTABA!)
+// 2. MOCK DE ASYNC STORAGE
 // Le damos a Jest la versión "de juguete" que viene incluida en la librería
 jest.mock('@react-native-async-storage/async-storage', () =>
     require('@react-native-async-storage/async-storage/jest/async-storage-mock')
 );
-
 
 describe('Pantalla Login', () => {
 
     it('debería mostrar los elementos principales de la pantalla', () => {
         render(<LoginScreen />);
 
-        expect(screen.getByText('Login')).toBeTruthy();
+        // VALIDAMOS LOS NUEVOS TEXTOS CENTRADOS DEL DISEÑO
+        expect(screen.getByText('Bienvenido')).toBeTruthy();
+        expect(screen.getByText('Inicia sesión para continuar')).toBeTruthy();
+        
+        // VALIDAMOS LOS INPUTS Y EL BOTÓN (Estos se mantuvieron igual)
         expect(screen.getByPlaceholderText('Correo Electrónico')).toBeTruthy();
         expect(screen.getByPlaceholderText('Contraseña')).toBeTruthy();
         expect(screen.getByText('Iniciar Sesión')).toBeTruthy();
